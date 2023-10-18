e11e6ab3fe11f9849370c9039fd116ac45a37a16
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-10-18 14:30:59 +0200

    feat: Add new examples

--- src/main/kotlin/io/github/filippovissani/FlowExamples.kt
+++ src/main/kotlin/io/github/filippovissani/FlowExamples.kt
@@ -3,6 +3,7 @@ package io.github.filippovissani
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 // https://kotlinlang.org/docs/flow.html#flow-builders
 
@@ -58,4 +59,31 @@ object FlowExamples {
             println("Finally in numbers")
         }
     }
+
+    fun log(msg: String) = println("[${Thread.currentThread().name}] $msg")
+
+    fun simple6(): Flow<Int> = flow {
+        log("Started simple flow")
+        for (i in 1..3) {
+            emit(i)
+        }
+    }
+
+    fun simple7(): Flow<Int> = flow {
+        // The WRONG way to change context for CPU-consuming code in flow builder
+        kotlinx.coroutines.withContext(Dispatchers.Default) {
+            for (i in 1..3) {
+                Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+                emit(i) // emit next value
+            }
+        }
+    }
+
+    fun simple8(): Flow<Int> = flow {
+        for (i in 1..3) {
+            Thread.sleep(100) // pretend we are computing it in CPU-consuming way
+            log("Emitting $i")
+            emit(i) // emit next value
+        }
+    }.flowOn(Dispatchers.Default) // RIGHT way to change context for CPU-consuming code in flow builder
 }
