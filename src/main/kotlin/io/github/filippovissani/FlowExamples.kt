7a74b24bce3197bc54b6afe20d8c6806e67d2afe
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-10-18 12:23:19 +0200

    feat: Add new examples

--- src/main/kotlin/io/github/filippovissani/FlowExamples.kt
+++ src/main/kotlin/io/github/filippovissani/FlowExamples.kt
@@ -4,6 +4,8 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
+// https://kotlinlang.org/docs/flow.html#flow-builders
+
 object FlowExamples {
     // This code outputs 1, 2, 3, but it waits 1000ms before printing each one.
     fun simple1(): Sequence<Int> = sequence { // sequence builder
@@ -30,6 +32,30 @@ object FlowExamples {
         for (i in 1..3) {
             delay(1000)
             emit(i)
+        }
+    }
+
+    fun simple5(): Flow<Int> = flow {
+        for (i in 1..3) {
+            delay(100)
+            println("Emitting $i")
+            emit(i)
+        }
+    }
+
+    suspend fun performRequest(request: Int): String {
+        delay(1000) // imitate long-running asynchronous work
+        return "response $request"
+    }
+
+    fun numbers(): Flow<Int> = flow {
+        try {
+            emit(1)
+            emit(2)
+            println("This line will not execute")
+            emit(3)
+        } finally {
+            println("Finally in numbers")
         }
     }
 }
